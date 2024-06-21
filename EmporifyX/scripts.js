b7d2388b0ec3a34beea19a376a5f8a6355c76fb7
Neeraj Kumar Malviya <105154959+Neeraj-Kumar-Malviya@users.noreply.github.com>
2024-06-21 18:32:14 +0530

    Add files via upload

---
+++ EmporifyX/scripts.js
@@ -0,0 +1,11 @@
+// document.getElementById('loginForm').addEventListener('submit', function(e) {
+//     e.preventDefault();
+//     // Add your login logic here
+//     alert('Login form submitted');
+// });
+
+document.getElementById('registerForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    // Add your registration logic here
+    alert('Register form submitted');
+});
